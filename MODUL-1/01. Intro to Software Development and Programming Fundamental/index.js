a56b637d5f0390529668937568b2106789e632c5
RadidDesfandri <radietdesfandri@gmail.com>
2025-11-16 10:03:53 +0700

    Init

---
+++ MODUL-1/01. Intro to Software Development and Programming Fundamental/index.js
@@ -0,0 +1,57 @@
+console.log("Hello world");
+
+// Variable let
+let message;
+console.log(message);
+message = "Hello";
+console.log(message);
+
+// Var global variable
+
+console.log(globalVariable);
+
+// Variable Const
+const constantVariable = 1 == 2
+console.log(typeof constantVariable);
+
+console.log(constantVariable);
+
+if (true) {
+  var globalVariable = "Global varible";
+
+  let data = "";
+}
+
+// data; // -> Akan error karena terkena scoop
+globalVariable;
+
+// String itu ditandai dengan ``, '', ""
+// Number ditandai dengan angka(1,2,3,5, ...)
+// Boolean ditandai dengan -> true atau false
+// Null -> kosong
+
+// Object
+const user = {
+  name: "radid",
+  address: "jakarta",
+  age: 99,
+  isWoman: true,
+  bio: null,
+};
+
+console.log(typeof user);
+
+// Array -> Bisa memasukkan banyak jenis data
+const users = [12, "12", false, true, user];
+console.log(typeof users);
+
+console.log(typeof []);
+console.log(typeof {});
+
+
+
+function convertDate(date) {
+
+}
+
+convertDate(false)
