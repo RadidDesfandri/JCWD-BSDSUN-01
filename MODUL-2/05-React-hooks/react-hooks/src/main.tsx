121f9f05a9f1871830782a3d488696d7a13e1c98
RadidDesfandri <radietdesfandri@gmail.com>
2026-01-18 16:03:10 +0700

    React hooks

---
+++ MODUL-2/05-React-hooks/react-hooks/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import ContextPage from "./pages/context.page.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/context" element={<ContextPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
