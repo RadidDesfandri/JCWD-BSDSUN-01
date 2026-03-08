3d553c8d84a75e6cdaf61db029a691ded2a4285f
RadidDesfandri <radietdesfandri@gmail.com>
2026-03-08 16:14:34 +0700

    feat: add CORS support to the Express application and update dependencies

--- MODUL-3/04-Auth-api/src/index.ts
+++ MODUL-3/04-Auth-api/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import authRouter from "./routes/auth.route";
 import postRouter from "./routes/post.route";
+import cors from "cors";
 
 const app: Application = express();
 const PORT = 8080;
 
 app.use(express.json());
+app.use(cors());
 
 // TESTING
 app.get("/ping", (req: Request, res: Response) => {
