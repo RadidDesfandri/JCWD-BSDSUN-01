a822e92403303af0bcc3aa349c00f6de5b3f4284
RadidDesfandri <radietdesfandri@gmail.com>
2026-03-08 13:54:51 +0700

    feat: implement CRUD operations for posts with Prisma integration

--- MODUL-3/04-Auth-api/src/controllers/post.controller.ts
+++ MODUL-3/04-Auth-api/src/controllers/post.controller.ts
@@ -1,10 +1,187 @@
 import { Request, Response } from "express";
+import prisma from "../config/prisma";
 
 const PostController = {
   create: async (req: Request, res: Response) => {
+    const { text } = req.body;
+
+    if (!text) {
+      res.status(400).send({
+        message: "Text wajib diisi!",
+      });
+    }
+
     try {
+      const post = await prisma.post.create({
+        data: {
+          text,
+          userId: req.user?.id,
+        },
+      });
+
       res.json({
         message: "create post",
+        data: post,
+      });
+    } catch (error) {
+      res.json({
+        message: "error",
+        error,
+      });
+    }
+  },
+  update: async (req: Request, res: Response) => {
+    const { text } = req.body;
+    const { postId } = req.params;
+
+    if (!text) {
+      res.status(400).send({
+        message: "Text wajib diisi!",
+      });
+    }
+
+    try {
+      // Periksa apakah postnya ada didatabase -> cari berdasarkan postId
+      const post = await prisma.post.findUnique({
+        where: {
+          id: Number(postId),
+        },
+      });
+
+      if (!post) {
+        return res.status(404).send({
+          message: "Post tidak ditemukan",
+        });
+      }
+
+      // Periksa apakah postnya milik user yang sedang login -> cari berdasarkan userId
+      if (post?.userId !== req.user?.id) {
+        return res.status(403).send({
+          message: "Forbidden",
+        });
+      }
+
+      const updatedPost = await prisma.post.update({
+        where: {
+          id: Number(postId),
+        },
+        data: {
+          text,
+        },
+      });
+
+      res.json({
+        message: "update post",
+        data: updatedPost,
+      });
+    } catch (error) {
+      res.json({
+        message: "error",
+        error,
+      });
+    }
+  },
+  delete: async (req: Request, res: Response) => {
+    const { postId } = req.params;
+
+    try {
+      const post = await prisma.post.findUnique({
+        where: {
+          id: Number(postId),
+        },
+      });
+
+      if (!post) {
+        return res.status(404).send({
+          message: "Post tidak ditemukan",
+        });
+      }
+
+      if (post?.userId !== req.user?.id) {
+        return res.status(403).send({
+          message: "Forbidden",
+        });
+      }
+
+      await prisma.post.delete({
+        where: {
+          id: Number(postId),
+        },
+      });
+
+      res.json({
+        message: "delete post",
+      });
+    } catch (error) {
+      res.json({
+        message: "error",
+        error,
+      });
+    }
+  },
+  all: async (req: Request, res: Response) => {
+    try {
+      const posts = await prisma.post.findMany({
+        include: {
+          user: {
+            select: {
+              id: true,
+              username: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      res.json({
+        message: "all post",
+        data: posts,
+      });
+    } catch (error) {
+      res.json({
+        message: "error",
+        error,
+      });
+    }
+  },
+  getPostByUserId: async (req: Request, res: Response) => {
+    try {
+      const { userId } = req.params;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: Number(userId),
+        },
+      });
+
+      if (!user) {
+        return res.status(404).send({
+          message: "User tidak ditemukan",
+        });
+      }
+
+      const posts = await prisma.post.findMany({
+        where: {
+          userId: Number(userId),
+        },
+        include: {
+          user: {
+            select: {
+              id: true,
+              username: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      res.json({
+        message: "all post user",
+        data: posts,
       });
     } catch (error) {
       res.json({
--- MODUL-3/04-Auth-api/src/routes/post.route.ts
+++ MODUL-3/04-Auth-api/src/routes/post.route.ts
@@ -6,4 +6,19 @@ const router = Router();
 
 router.post("/create", AuthMiddleware.verifyToken, PostController.create);
 
+router.patch(
+  "/update/:postId",
+  AuthMiddleware.verifyToken,
+  PostController.update,
+);
+
+router.delete(
+  "/delete/:postId",
+  AuthMiddleware.verifyToken,
+  PostController.delete,
+);
+
+router.get("/all", PostController.all);
+router.get("/user/:userId", PostController.getPostByUserId);
+
 export default router;
